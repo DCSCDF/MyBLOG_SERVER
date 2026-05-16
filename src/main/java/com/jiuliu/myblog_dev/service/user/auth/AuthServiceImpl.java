c347db3cf2c3ff338e72852d5ee11199580b4c6f
JiuLiutop <3209174373@qq.com>
2026-05-16 22:50:41 +0800

    auth检查频率限制

--- src/main/java/com/jiuliu/myblog_dev/service/user/auth/AuthServiceImpl.java
+++ src/main/java/com/jiuliu/myblog_dev/service/user/auth/AuthServiceImpl.java
@@ -112,17 +112,6 @@ public class AuthServiceImpl implements AuthService {
         this.pendingPasswordResetService = pendingPasswordResetService;
     }
 
-    // 400: '请求参数错误',
-    // 401: '未授权，请重新登录',
-    // 403: '拒绝访问',
-    // 404: '请求的资源不存在',
-    // 408: '请求超时',
-    // 429: '请求过于频繁',
-    // 500: '服务器内部错误',
-    // 502: '网关错误',
-    // 503: '服务不可用',
-    // 504: '网关超时'
-
     @Override
     public SaResult getPublicKey() {
         Map<String, Object> data = new HashMap<>();
